2792b7b2556911fc77675e9e4bb30c5ca0b0c2d6
Michal Frankiewicz <=>
2023-12-17 21:28:35 +0100

    add test for user data

--- be/tests/Feature/HelpersTrait.php
+++ be/tests/Feature/HelpersTrait.php
@@ -2,8 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Enums\Game\GameStatusEnum;
+use App\Enums\Score\ScoreStatusEnum;
 use App\Models\Card;
+use App\Models\Game;
+use App\Models\Round;
+use App\Models\Score;
 use App\Models\User;
+use Carbon\Carbon;
 use Database\Seeders\CardSeeder;
 use Database\Seeders\LevelSeeder;
 
@@ -33,7 +39,7 @@ trait HelpersTrait
         }
     }
 
-    protected function prepareSetUp()
+    protected function prepareSetUp(): void
     {
         // const value for number of rounds
         \Config::set('game.rounds.max_rounds', self::MAX_ROUNDS);
@@ -41,5 +47,84 @@ trait HelpersTrait
 
         $this->seed(CardSeeder::class);
         $this->seed(LevelSeeder::class);
+    }
+
+    private function generateDummyGameFinishedUserAlwaysWins(User $user): Game
+    {
+        $opponent = User::factory()->create();
+        $this->prepareDeckCards($opponent);
+
+        $game = Game::factory()->create([
+            'valid_until' => Carbon::now(),
+            'finished_at' => Carbon::now()->addMinutes(),
+            'status' => GameStatusEnum::FINISHED,
+        ]);
+
+        $game->addPlayer($user->player);
+        $game->addPlayer($opponent->player);
+
+        $game->save();
+
+        $opponentPoints = 0;
+        $userPoints = 0;
+
+        for ($i = 0; $i < self::MAX_ROUNDS; $i++) {
+
+            /** @var Round $round */
+            $round = $game->rounds()->create([
+                'round_number' => $i + 1,
+            ]);
+
+            $cardFromUser = $user->player->deck->deckCards->random();
+            $cardFromOpponent = $opponent->player->deck->deckCards->random();
+
+            $round->moves()->create([
+                'player_id' => $opponent->player->id,
+                'deck_card_id' => $cardFromOpponent->id,
+                'points' => $cardFromOpponent->card->power,
+            ]);
+
+            $opponentPoints += $cardFromOpponent->card->power;
+
+            $round->moves()->create([
+                'player_id' => $user->player->id,
+                'deck_card_id' => $cardFromUser->id,
+                'points' => $cardFromUser->card->power * 1000,
+            ]);
+
+            $userPoints += $cardFromUser->card->power;
+        }
+
+        if ($userPoints > $opponentPoints) {
+            Score::create([
+                'player_id' => $user->player->id,
+                'status' => ScoreStatusEnum::WON,
+                'game_id' => $game->id,
+                'points' => $userPoints,
+            ]);
+
+            Score::create([
+                'player_id' => $opponent->player->id,
+                'status' => ScoreStatusEnum::DEFEAT,
+                'game_id' => $game->id,
+                'points' => $opponentPoints,
+            ]);
+        } else {
+            Score::create([
+                'player_id' => $user->player->id,
+                'status' => ScoreStatusEnum::DEFEAT,
+                'game_id' => $game->id,
+                'points' => $userPoints,
+            ]);
+
+            Score::create([
+                'player_id' => $opponent->player->id,
+                'status' => ScoreStatusEnum::WON,
+                'game_id' => $game->id,
+                'points' => $opponentPoints,
+            ]);
+        }
+
+        return $game;
     }
 }
